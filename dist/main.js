25c17c0fca1cb4b59f80f30beef5cd06d52e812d
Matheus Bortoletto <matheus@suaquadra.com.br>
2024-03-06 22:09:45 -0300

    log

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,10 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    console.log(`Server is running on 
+    ${process.env.PORT || 3000},
+    ${process.env.HOST || "localhost"}`);
+    await app.listen(process.env.PORT || 3000, process.env.HOST || "localhost");
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
