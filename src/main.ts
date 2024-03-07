e26fd7982f3e1665b4fc15ecc900b9261f26e928
Matheus Bortoletto <matheus@suaquadra.com.br>
2024-03-06 22:13:20 -0300

    port/host

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(
-    `Server is running on 
-    ${process.env.PORT || 3000},
-    ${process.env.HOST || "localhost"}`
-  );
-  await app.listen(process.env.PORT || 3000, process.env.HOST || "localhost");
+  console.log(`Server is running on ${port}:${host}`);
+  await app.listen(port, host);
 }
 bootstrap();
