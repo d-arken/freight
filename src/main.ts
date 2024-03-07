15856d8d65d30f93e3ac30b3cbf60dbccc673a37
Matheus Bortoletto <matheus@suaquadra.com.br>
2024-03-06 22:05:19 -0300

    log

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(
+    `Server is running on 
+    ${process.env.PORT || 3000},
+    ${process.env.HOST || "localhost"}`
+  );
   await app.listen(process.env.PORT || 3000, process.env.HOST || "localhost");
 }
 bootstrap();
